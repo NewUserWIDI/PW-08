cc3b354e6325960d0cdeb54e86b3b04bc6266546
wwidi <wwidi4087@gmail.com>
2025-10-28 14:50:14 +0700

    update

--- proses_upload.php
+++ proses_upload.php
@@ -26,3 +26,40 @@ if (isset($_FILES['files']['name'][0])) {
     echo "Tidak ada file yang diunggah.";
 }
 ?>
+
+<?php
+// Lokasi penyimpanan file gambar
+$targetDirectory = "uploads/";
+
+// Membuat folder uploads jika belum ada
+if (!file_exists($targetDirectory)) {
+    mkdir($targetDirectory, 0777, true);
+}
+
+// Periksa apakah ada file yang diunggah
+if (!empty($_FILES['images']['name'][0])) {
+    $totalFiles = count($_FILES['images']['name']);
+    $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+
+    // Loop untuk setiap file gambar
+    for ($i = 0; $i < $totalFiles; $i++) {
+        $fileName = $_FILES['images']['name'][$i];
+        $tmpName = $_FILES['images']['tmp_name'][$i];
+        $fileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+        $targetFile = $targetDirectory . basename($fileName);
+
+        // Validasi tipe file
+        if (in_array($fileType, $allowedExtensions)) {
+            if (move_uploaded_file($tmpName, $targetFile)) {
+                echo "File <b>$fileName</b> berhasil diunggah.<br>";
+            } else {
+                echo "Gagal mengunggah file <b>$fileName</b>.<br>";
+            }
+        } else {
+            echo "File <b>$fileName</b> tidak diizinkan. Hanya file gambar yang diizinkan.<br>";
+        }
+    }
+} else {
+    echo "Tidak ada file yang diunggah.";
+}
+?>
